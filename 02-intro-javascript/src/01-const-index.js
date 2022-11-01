0cd299981a0d526fd575d79687502386ff5db0a5
Erik Villegas <erdvillegas@gmail.com>
2022-11-01 01:06:30 -0600

    Clase intro de JS

---
+++ 02-intro-javascript/src/01-const-index.js
@@ -0,0 +1,17 @@
+
+
+// Variables y Constantes
+const nombre = 'Erik';
+const apellido = "Villegas";
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre,apellido, valorDado);
+
+if (true) {
+    let valorDado = 6;
+    console.log(valorDado);
+}
+
+console.log(valorDado);

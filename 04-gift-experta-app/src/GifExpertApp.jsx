6ad1c891c2fc057ac60a26a69adee4c4c6f8a2c2
Erik Villegas <erdvillegas@gmail.com>
2023-02-04 01:49:30 -0600

    Agregando componente de Grid

--- 04-gift-experta-app/src/GifExpertApp.jsx
+++ 04-gift-experta-app/src/GifExpertApp.jsx
@@ -1,33 +1,28 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import GifGrid from "./components/GifGrid";
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['The Legend Of Zelda', 'Silent Hill 2']);
 
-    const onAddCategory = ( newCategory ) => {
+    const onAddCategory = (newCategory) => {
 
-        if ( categories.includes( newCategory ) ) return;
+        if (categories.includes(newCategory)) return;
         setCategories([newCategory, ...categories]);
     };
 
     return (
         <>
-
-            {/* Titulo */}
             <h1>GifExpertApp</h1>
 
-            {/* Input */}
             <AddCategory
                 onNewCategory={onAddCategory}
             />
-
-            {/* Listado de Gif */}
-            <ol>
-                {categories.map( category => {
-                    return <li key={category}>{category}</li>
-                })}
-            </ol>
-            {/* Gif Item */}
+            {
+                categories.map((category) =>
+                    <GifGrid key={category} category={category} />
+                )
+            }
         </>
     )
 }

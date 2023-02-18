d9df0127e700f1f1921083af0b37057c3ced7471
Erik Villegas <erdvillegas@gmail.com>
2023-02-18 01:27:52 -0600

    Agregando pruebas

--- 04-gift-experta-app/tests/components/GifItem.test.jsx
+++ 04-gift-experta-app/tests/components/GifItem.test.jsx
@@ -1,15 +1,31 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe('Pruebas en <GifItem>', () => {
   
     const title = "Link";
-    const url = 'https://google.com/link';
+    const url = 'https://www.16personalities.com/static/images/personality-types/avatars/infj-advocate.png';
 
     test('debe hacer match con el snapshopt', () => {
         const { container } =  render(<GifItem title={title} url={url} />);
         expect( container ).toMatchSnapshot();
     });
     
+    test('debe de mostrar la imagen con el URL y el ALT indicado ', () => {
+        
+        const { container } =  render(<GifItem title={title} url={url} />);
+        // screen.debug();
+        // expect( screen.getByRole('img').src ).toBe( url );
 
+        const { src, alt } = screen.getByRole('img');
+        expect( src ).toBe( url );
+        expect( alt ).toBe( alt );
+    });
+
+    test('debe de mostrar el titulo en el componente', () => {
+      
+        const { container } =  render(<GifItem title={title} url={url} />);
+        expect( screen.getByText( title )).toBeTruthy();
+    });
+    
 });
